8402e5ebb297101031a3e16adcce61c92229584f
tylertran349 <tylertran349@gmail.com>
2023-03-09 22:22:51 -0800

    Add message field to the schema

--- models/message.js
+++ models/message.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Scehma;
+const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: "User", required: true},
     title: {type: String, required: true},
-    content: {type: String, required: true},
+    message: {type: String, required: true},
     time: {type: Date, required: true}
 });
 
