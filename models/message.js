ebf6315ee37c7c55a9366a3cf39c0133c3b827bc
tylertran349 <tylertran349@gmail.com>
2023-03-09 22:33:40 -0800

    Add date/time formatting via Luxon

--- models/message.js
+++ models/message.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const MessageSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: "User", required: true},
     title: {type: String, required: true},
     message: {type: String, required: true},
     time: {type: Date, required: true}
+});
+
+MessageSchema.virtual("time_formatted").get(function() {
+    return DateTime.fromJSDate(this.time).toLocaleString(DateTime.DATETIME_SHORT);
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
