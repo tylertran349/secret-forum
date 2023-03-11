d002a309cfbc9416e6a9bb0d64922ce90fcb840a
tylertran349 <tylertran349@gmail.com>
2023-03-10 22:19:29 -0800

    Add admin_status attribute

--- models/user.js
+++ models/user.js
@@ -7,6 +7,7 @@ const UserSchema = new Schema({
     username: {type: String, required: true},
     password: {type: String, required: true},
     membership_status: {type: Boolean, required: true},
+    admin_status: {type: Boolean, required: true},
     messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
 });
 
